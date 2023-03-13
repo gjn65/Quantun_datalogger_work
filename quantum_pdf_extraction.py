1d6218b77c33ced38980595f04e437d511ddd8db
gjn65 <rotax65@gmail.com>
2023-03-13 11:42:18 +1030

    Initial test

--- quantum_pdf_extraction.py
+++ quantum_pdf_extraction.py
@@ -5,13 +5,6 @@ from pypdf import PdfReader
 pp=pprint.PrettyPrinter(indent=4)
 
 def main():
-    ''' pdf_obj = open('test1.pdf','rb')
-    pdf_reader = PyPDF2.PdfReader(pdf_obj)
-    print(len(pdf_reader.pages))
-    page_obj = pdf_reader.pages[0]
-    print(page_obj.extract_text())
-    pdf_obj.close()
-    '''
 
     reader=PdfReader('test1.pdf')
     print(len(reader.pages))
