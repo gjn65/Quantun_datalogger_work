57c8cc5272db19f02be791acdffe22c9e29ab921
gjn65 <rotax65@gmail.com>
2023-03-14 16:52:35 +1030

    Speed adjustment done. Skip list done

--- quantum_pdf_extraction.py
+++ quantum_pdf_extraction.py
@@ -1,20 +1,52 @@
 import pprint
-import PyPDF2
 from pypdf import PdfReader
+import quantum_pdf_extraction_cfg as cfg
 
 pp=pprint.PrettyPrinter(indent=4)
 
 def main():
 
-    reader=PdfReader('test1.pdf')
+    reader=PdfReader(cfg.source_file)
     print(len(reader.pages))
     pages=len(reader.pages)
+    # Iterate through each page
     for page in range(pages):
         print("Processing page "+str(page))
         lines=reader.pages[page].extract_text().split('\n')
-        print(lines)
+        # Iterate through each line for this page
+        for line in lines:
+            print(line)
+            print(str(len(line)))
+            if len(line) == 0:
+                continue
+
+            # Search for wheel diameter figure in 1st page, but only if adjustment factor has not already been established
+            if page == 0:
+                if cfg.speed_adjustment_factor == 0:
+                    if "Circumference" in line and "Diameter" in line:
+                        words=line.split()
+                        pp.pprint(words)
+                        wheel_diameter_qdp_inches=float(words[-1])    # wheel diameter according to the QDP software
+                        cfg.speed_adjustment_factor = cfg.wheel_dia_actual_mm / (wheel_diameter_qdp_inches*25.4)
+                        pp.pprint(cfg.speed_adjustment_factor)
+                        continue
+                continue        # We don't want anything else from page 0
+
+            # Skip lines with strings we are not interested in
+            if skip_line_found(line) == True:
+                continue
 
 
+
+def skip_line_found(line):
+    '''
+        Search for existence of skip_list word(s) in the passed line.
+        If found then return True, other wise return False
+    '''
+    for word in cfg.skip_list_words:
+        if word in line:
+            return True
+    return False
 
 if __name__ == '__main__':
     main()
---
+++ quantum_pdf_extraction_cfg.py
@@ -0,0 +1,22 @@
+# File extacted from Quantum Desktop Software
+# - print to PDF file to generate.
+source_file='test1.pdf'
+
+# Wheel diameter in mm - this may be used to correct the speed calculated by the
+# QDP software which uses a figure embedded in the logger (which will be in inches)
+wheel_dia_actual_mm = 995
+
+# Speed adjustment factor.
+# This will be the actual wheel diameter divided by the QDP reported diameter (converted to mm)
+# If this sis set to 0 then the calculation has not yet been made and the code will assume a 1:1 ratio
+# The QDP wheel size is generally reported on Page 0 of the printout with a pair of lines as follows:
+# "Wheel size used by program" followed immediately by the line
+# Circumference = xxx.x Diameter = xx.x
+speed_adjustment_factor=0
+
+# Any line containing one of these phrases in omitted from processing
+# (as are all lines on Page 0 - with the exception of the wheel diameter line)
+skip_list_words = ["Quantum Desktop Playback",
+                   "Report Date",
+                   "Locomotive",
+                   'TIME']
